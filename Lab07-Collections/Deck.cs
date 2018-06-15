bf67294bbc0dfab8193cadb284047bf1ebc7373d
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-14 19:46:41 -0700

    Added code that isn't working.

---
+++ Lab07-Collections/Deck.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07_Collections
+{
+    public class Deck<T> : IEnumerable<T>
+    {
+
+        public  T[] cardDeck = new T[15];
+
+        public int count = 0;
+
+        //TODO: Add Card
+        public void Add(T newCard)
+        {
+            if (count == cardDeck.Length)
+            {
+                Array.Resize(ref cardDeck, cardDeck.Length * 2);
+            }
+
+            cardDeck[count++] = newCard;
+        }
+
+
+        //TODO: shuffle
+        //troubleshoot: cardDeck has duplicates and doubles the deck. 
+        public  T[] Shuffle()
+        {
+
+            for (int i = 0; i < count; i++)
+            {
+                Random randomNum = new Random();
+                int randomIndexNum = randomNum.Next(0, count);
+ 
+                cardDeck[i] = cardDeck[randomIndexNum];
+
+            }
+
+            return cardDeck;
+        }
+
+        //TODO: remove card
+        public void RemoveCard()
+        {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return cardDeck[i];
+            }
+        }
+
+        //Don't Touch
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public  Deck<Card> CreateDeck()
+        {
+            Deck<Card> newDeck = new Deck<Card>
+            {
+                new Card(CardFace.ace, Suit.Clubs),
+                new Card(CardFace.king, Suit.Clubs),
+                new Card(CardFace.queen, Suit.Clubs),
+                new Card(CardFace.jack, Suit.Clubs),
+                new Card(CardFace.ace, Suit.Hearts),
+                new Card(CardFace.king, Suit.Hearts),
+                new Card(CardFace.queen, Suit.Hearts),
+                new Card(CardFace.jack, Suit.Hearts),
+                new Card(CardFace.ace, Suit.Spades),
+                new Card(CardFace.king, Suit.Spades),
+                new Card(CardFace.queen, Suit.Spades),
+                new Card(CardFace.jack, Suit.Spades),
+                new Card(CardFace.ace, Suit.Diamonds),
+                new Card(CardFace.king, Suit.Diamonds),
+                new Card(CardFace.queen, Suit.Diamonds),
+                new Card(CardFace.jack, Suit.Diamonds)
+            };
+            return newDeck;
+        }
+
+
+
+
+    }
+}
--- Lab07-Collections/Program.cs
+++ Lab07-Collections/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using Lab07_Collections;
+
+
+
 
 namespace Lab07_Collections
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            HomeDisplay();
+            
+            //DisplayDeck(Deck<Card>.CreateDeck());
+            //Deck<Card>.Shuffle();
+            //DisplayDeck(Deck<Card>.CreateDeck());
+
         }
+        public static void HomeDisplay()
+        {
+            Console.WriteLine("Please Select an option below");
+            Console.WriteLine("1. Display Deck");
+            Console.WriteLine("2. Shuffle Deck");
+            Console.WriteLine("3. Add to the Deck");
+            Console.WriteLine("4. Remove from the deck");
+
+
+            //Added by fellow studend to replace Deck<Card> that was throwing errors.
+            Deck<Card> deck = new Deck<Card>();
+
+
+            int userResponse = Convert.ToInt32(Console.ReadLine());
+            if(userResponse == 1)
+            {
+                DisplayDeck(deck.CreateDeck());
+            }
+            if (userResponse == 2)
+            {
+                deck.Shuffle();
+                DisplayDeck(deck.CreateDeck());
+            }
+            if (userResponse == 3)
+            {
+                deck.Add(new Card(CardFace.two, Suit.Clubs));
+                DisplayDeck(deck.CreateDeck());
+            }
+        }
+
+        public static void DisplayDeck(Deck<Card> deck)
+        {
+            foreach(Card card in deck)
+            {
+                Console.WriteLine($"{card.CardFace} of {card.Suit}");
+            }
+            Console.WriteLine($"{deck.count} number of cards in the deck");
+        }
+
     }
 }
